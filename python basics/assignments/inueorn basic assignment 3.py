26b7979f4478fca216f0f431f278be6bf1ea5184
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-04-17 19:47:34 -0400

    Add files via upload

---
+++ python basics/assignments/inueorn basic assignment 3.py
@@ -0,0 +1,14 @@
+Why are functions advantageous to have in your programs? reusability
+When does the code in a function run: when it's specified or when it's called? When it is called
+What statement creates a function? def
+What is the difference between a function and a function call? Function describes what task it does where as it will be executed only when it is called
+How many global scopes are there in a Python program? How many local scopes?
+What happens to variables in a local scope when the function call returns? They are deleted
+What is the concept of a return value? Is it possible to have a return value in an expression? Statements written after the return statement will not be executed
+If a function does not have a return statement, what is the return value of a call to that function? If a function does not have return statement then its return value is void
+How do you make a function variable refer to the global variable? Use the keyword global
+What is the data type of None? The data type of None is nonetype
+What does the sentence import areallyourpetsnamederic do? It will import the module areallyourpetsnamederic and runs the init.py file
+If you had a bacon() feature in a spam module, what would you call it after importing spam? import spam as np np.bacon()
+What can you do to save a programme from crashing if it encounters an error? place the code that might create an exception in the try block and then use except block to handle it
+What is the purpose of the try clause? What is the purpose of the except clause? place the code that might create an exception in the try block and then use except block to hanfle it
