f738ca42687c56c78c59973c6d28aaa72f54a83d
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-04-17 19:41:36 -0400

    Add files via upload

---
+++ python basics/assignments/basic assignment 1 inueorn.py
@@ -0,0 +1,39 @@
+In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+expression 'hello' value
+87.8 value
+expression / expression
+expression 6 value
+What is the difference between string and variable? a varaible is used to store data .this data can be of any type for example it can be string , int ,boolean string is a type of data when we want to store a string in variable we put it in either '' or ""
+Describe three different data types. Int Float bool
+
+What is an expression made up of? What do all expressions do? Expression are made up of operators which act on operands.they evaluate to a value
+
+This assignment statements, like spam = 10. What is the difference between an expression and a statement? Expression evaluates to value unlike a statement
+
+After running the following code, what does the variable bacon contain? bacon = 22 bacon + 1 22
+bacon =22
+bacon+1
+print(bacon)
+22
+What should the values of the following two terms be? 'spam' + 'spamspam' =’spamspamspam’ 'spam' * 3 =’spamspamspam’
+
+Why is eggs a valid variable name while 100 is invalid? In python variable name cannot start from number
+
+What three functions can be used to get the integer, floating-point number, or string version of a value? Int() Float() Str()
+
+Why does this expression cause an error? How can you fix it? 'I have eaten ' + 99 + ' burritos.'=integer cannot be added to a string Solution to this is ‘I have eater’+’99’+’burritos’
+
+'I have eaten ' + 99 + ' burritos.'
+---------------------------------------------------------------------------
+TypeError                                 Traceback (most recent call last)
+<ipython-input-5-d24137131a5c> in <module>
+----> 1 'I have eaten ' + 99 + ' burritos.'
+
+TypeError: can only concatenate str (not "int") to str
+
+'i have eaten'+' 99 '+'burritos'
+'i have eaten 99 burritos'
+'spam'+'spamspam'
+'spamspamspam'
+'spam'*3
+'spamspamspam
