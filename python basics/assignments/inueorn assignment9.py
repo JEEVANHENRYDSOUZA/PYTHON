5f35f17592dec7844f1cc0293a570bd18669816f
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-04-20 15:49:51 -0400

    Create inueorn assignment9.py

---
+++ python basics/assignments/inueorn assignment9.py
@@ -0,0 +1,20 @@
+1.	To what does a relative path refer?
+Relative path refers to a the path relative to some file or folder ,it is not the absolute path
+2.	What does an absolute path start with your operating system?
+\
+3. What do the functions os.getcwd() and os.chdir() do?
+4. What are the . and .. folders?
+. represents the current  folder and .. represents the folder  above the current folder
+5. In C:\bacon\eggs\spam.txt, which part is the dir name, and which part is the base name?
+C:\bacon\eggs----dir nam
+Spam.txt base name
+6. What are the three “mode” arguments that can be passed to the open() function?
+r ,w ,r+
+7. What happens if an existing file is opened in write mode?
+It will overwrite contents
+8. How do you tell the difference between read() and readlines()?
+readlines, which returns a list of lines
+read() reads the entire contents of the file into a string. You can also give read() an optional parameter which represents the number of characters to be read from the file
+9. What data structure does a shelf value resemble?
+It represents a dictionary
+
