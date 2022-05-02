3f2a261b00b04bf6ddcae5e4783041a447ef9c50
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-05-02 16:08:42 -0400

    Create inueorn basic assignment 22.py

---
+++ python basics/assignments/inueorn basic assignment 22.py
@@ -0,0 +1,20 @@
+Answers:
+Question1:
+‘iNueron’----gllbal declration
+
+Question 2:
+Output: ‘iNueron’
+
+Question 3:
+‘NI!’-local scope is preferred 
+‘iNueron’-> after function execution it goes for garbage collection
+
+Question4:
+‘NI’declared as global
+
+Question 5:
+‘NI’local scope
+‘INeuron’->global scope
+
+Question 6:
+Output:’Spams’
