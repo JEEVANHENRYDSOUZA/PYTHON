1cd80c4d1e97c67a56c0cbcef6353f2076086e24
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-05-17 12:55:21 -0400

    Create inueron advance assignment 6.py

---
+++ python oops/assignments/inueron advance assignment 6.py
@@ -0,0 +1,24 @@
+Q1. Describe three applications for exception processing.
+The three applications for exception processing are as
+1.to check valid input
+2.to check file i/o operations
+3.to check arithmetic exceptions like divide by zero while performing mathematical operations
+
+
+
+Q2. What happens if you don't do something extra to treat an exception?
+The program execution will stop at that line where the exception occurred and line written below it will never be executed
+
+
+Q3. What are your options for recovering from an exception in your script?
+Try and except
+
+Q4. Describe two methods for triggering exceptions in your script.
+1. raise method triggers an exception if condition provided to it turns out to be True.
+2. assert will let the program to continue execution if condition provided to it turns out to be True else exception will be raised
+
+
+
+Q5. Identify two methods for specifying actions to be executed at termination time, regardless of whether or not an exception exists.
+
+Else and finally are the two methods
