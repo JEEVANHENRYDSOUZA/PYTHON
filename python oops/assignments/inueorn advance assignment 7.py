5255d65a116f6d6e3681c58c27d6fa5321482ca8
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-05-18 15:18:30 -0400

    Create inueorn advance assignment 7.py

---
+++ python oops/assignments/inueorn advance assignment 7.py
@@ -0,0 +1,21 @@
+‘Q1. What is the purpose of the try statement?
+In  the try statement we will write  the code in which we want to check the exception
+
+
+
+Q2. What are the two most popular try statement variations?
+
+1.Try except else 
+2,try except finally 
+Are the two variations of try block
+
+Q3. What is the purpose of the raise statement?
+Raise is like a force full statement which handle only the exception written ahead of it
+
+
+Q4. What does the assert statement do, and what other statement is it like?
+Assert are like Boolean statements they will work properly when the Boolean value returned is true and will throw an error If the Boolean value returned is false
+
+
+Q5. What is the purpose of the with/as argument, and what other statement is it like?
+With statement is used for resource management and exception handling
