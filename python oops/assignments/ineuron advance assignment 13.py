38bd9c118379a7583a3b1bf2e9ed4c168800afc4
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-05-20 20:55:27 -0400

    Create ineuron advance assignment 13.py

---
+++ python oops/assignments/ineuron advance assignment 13.py
@@ -0,0 +1,40 @@
+Q1. Does assigning a value to a string's indexed character violate Python's string immutability?
+
+yes it violates python string immutabillity
+
+Q2. Does using the += operator to concatenate strings violate Python's string immutability? Why or why not?
+It creates a new string object
+
+
+Q3. In Python, how many different ways are there to index a character?
+There  are two ways one is using positive  indexing and second is using negative indexing
+
+
+Q4. What is the relationship between indexing and slicing?
+To access the elements of the string we can use indexing and slicing
+
+
+Q5. What is an indexed character's exact data type? What is the data form of a slicing-generated substring?
+
+Of an indexed character the exact data type is:
+<class 'str'>
+<class 'str'>
+
+
+Q6. What is the relationship between string and character "types" in Python?
+Object that contains a sequence of character is called as string
+
+
+Q7. Identify at least two operators and one method that allow you to combine one or more smaller strings to create a larger string.
++,*
+
+
+Q8. What is the benefit of first checking the target string with in or not in before using the index method to find a substring?
+If we do not check the target string with in or not in then we will get an exception called as Valueerror
+
+
+Q9. Which operators and built-in string methods produce simple Boolean (true/false) results?
+ In, not in 
+ String,isupper()
+String.islower()
+String.isalpha() etc
