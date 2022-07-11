543644c473194cdef42674d9f682a6bd7d56f10b
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-07-11 14:32:12 -0400

    Create 1720 decode encoded array xorred.py

---
+++ python leetcode/1720 decode encoded array xorred.py
@@ -0,0 +1,7 @@
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        r = [first]
+        for i in encoded:
+            r.append(r[-1]^i)
+        return r
+        
